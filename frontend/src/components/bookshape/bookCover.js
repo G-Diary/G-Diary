d17321a0f04659acf6aaf0af3c30ac1d1de8b07c
gs0428 <lider28@naver.com>
2023-01-06 14:49:57 +0900

    Style : 다이어리 표지 및 배경 이미지

---
+++ frontend/src/components/bookshape/bookCover.js
@@ -0,0 +1,13 @@
+import { Container } from '@material-ui/core';
+import React from 'react';
+
+export default function BookCover() {
+  return (
+    <div>
+      <div className='left'></div>
+      <div className='right'>
+        <div className='year'>2023</div>
+      </div>
+    </div>
+  );
+}
