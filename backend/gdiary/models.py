e58b53e2bae2fc82f4094a1ea71017dd306c4d48
Heekyeong Park <qkrgmlrud00@gmail.com>
2023-01-16 21:28:06 -0800

    feat: base model 생성 및 serializer 작성

--- backend/gdiary/models.py
+++ backend/gdiary/models.py
@@ -1,19 +1,24 @@
 from django.db import models
 
-class User(models.Model):
-    id = models.AutoField(primary_key=True) #pk
-    email = models.EmailField(max_length=50, null=False, unique=True)
-    nickname = models.CharField(max_length=10, null=False)
-    #password = models.CharField(null=False)
-    cover_image_url = models.CharField(max_length=500, null=False)
+class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, null=False)
     updated_at = models.DateTimeField(auto_now=True, null=False)
-    is_deleted = models.BooleanField(null=False)
+    is_deleted = models.BooleanField(default=False, null=False)
+
+    class Meta:
+        abstract=True # 이 모델은 데이터베이스 테이블을 만드는데 사용되지 않겠다.
+
+class User(BaseModel):
+    id = models.AutoField(primary_key=True) #pk
+    email = models.EmailField(max_length=50, null=False)
+    nickname = models.CharField(max_length=10, null=False)
+    cover_image_url = models.CharField(max_length=500, null=False)
+
 
     def __str__(self):
         return self.id
 
-class Diary(models.Model):
+class Diary(BaseModel):
     id = models.AutoField(primary_key=True) #pk
     user_id = models.ForeignKey(User, on_delete=models.CASCADE, null=False) #fk
     title = models.CharField(max_length=50, null=False)
@@ -21,31 +26,22 @@ class Diary(models.Model):
     drawing_url = models.CharField(max_length=500, null=False)
     contents = models.CharField(max_length=50, null=False)
     diary_date = models.DateField(null=False)
-    created_at = models.DateTimeField(auto_now_add=True, null=False)
-    updated_at = models.DateTimeField(auto_now=True, null=False)
-    is_deleted = models.BooleanField(null=False)
+ 
 
     def __str__(self):
         return self.id
 
 
-class Keyword(models.Model):
-    id = models.AutoField(primary_key=True) #pk
-    keyword = models.CharField(max_length=10, null=False)
-    created_at = models.DateTimeField(auto_now_add=True, null=False)
-    updated_at = models.DateTimeField(auto_now=True, null=False)
-    is_deleted = models.BooleanField(null=False)
+class Keyword(BaseModel):
+    keyword = models.CharField(max_length=10, null=False) #pk
 
     def __str__(self):
         return self.keyword
 
-class Drawing(models.Model):
+class Drawing(BaseModel):
     id = models.AutoField(primary_key=True) #pk
     keyword = models.ForeignKey(Keyword, on_delete=models.CASCADE, null=False) #fk
     image_url = models.CharField(max_length=500, null=False)
-    created_at = models.DateTimeField(auto_now_add=True, null=False)
-    updated_at = models.DateTimeField(auto_now=True, null=False)
-    is_deleted = models.BooleanField(null=False)
 
     def __str__(self):
         return self.id
