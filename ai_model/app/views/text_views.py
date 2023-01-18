c66fa1b23d1ef8965ecbb735e695f8c9faf9a73e
aristo <dkfud2121@naver.com>
2023-01-18 18:44:20 +0900

    feat : ai module in django

--- ai_model/app/views/text_views.py
+++ ai_model/app/views/text_views.py
@@ -4,40 +4,24 @@ from konlpy.tag import Kkma
 kkma = Kkma()
 bp = Blueprint('main', __name__, url_prefix='/')
 
-diaries = [
-    {
-        'id': 1,
-        'title': '오늘은 즐거운 날이다.',
-        'contents': '아침에 먹은 사과가 맛있다. 짱이다.',
-        'is_deleted': False
-    },
-    {
-        'id': 2,
-        'title': '오늘은 슬픈 날이다.',
-        'contents': '피아노를 치는데 소리가 구리다. 자괴감이 대박이다.',
-        'is_deleted': False
-    },
-]
-
-
-@bp.route('/api/v1/diaries/<int:diary_id>', methods=['GET'])
-def get_keyword(diary_id):
-    for diary in diaries:
-        if diary['id'] == diary_id:
-            get_diary = diary
-    if len(get_diary) == 0:
+# @bp.route('/api/v1/diaries/<int:diary_id>', methods=['GET'])
+def get_keyword(diary_contents):
+    # for diary in diaries:
+    #     if diary['id'] == diary_id:
+    #         get_diary = diary
+    if len(diary_contents) == 0:
         abort(404)
 
-    diary_keyword = kkma.nouns(diary['contents'])
+    diary_keyword = kkma.nouns(diary_contents)
     return_key = {"keyword": diary_keyword}
     return return_key
 
-
-@bp.route('/hello')
-def hello_pybo():
-    return 'Hello, Pybo!'
-
-
-@bp.route('/')
-def hello_index():
-    return 'index'
+#
+# @bp.route('/hello')
+# def hello_pybo():
+#     return 'Hello, Pybo!'
+#
+#
+# @bp.route('/')
+# def hello_index():
+#     return 'index'
--- backend/text/views.py
+++ backend/text/views.py
@@ -2,5 +2,62 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from backend.text.models import Diary
+from ai_model.app.views.text_views import get_keyword
+
+
 def hello_world(request):
     return HttpResponse("hello world! this is text api")
+
+
+diaries = [
+    {
+        'id': 1,
+        'title': '오늘은 즐거운 날이다.',
+        'contents': '아침에 먹은 사과가 맛있다. 짱이다.',
+        'is_deleted': False
+    },
+    {
+        'id': 2,
+        'title': '오늘은 슬픈 날이다.',
+        'contents': '피아노를 치는데 소리가 구리다. 자괴감이 대박이다.',
+        'is_deleted': False
+    },
+]
+
+# def new_post(request):
+    # if request.method == 'POST':
+    #     if request.POST['mainphoto']:
+    #         new_article=Post.objects.create(
+    #             postname=request.POST['postname'],
+    #             contents=request.POST['contents'],
+    #             mainphoto=request.POST['mainphoto'],
+    #         )
+    #     else:
+    #         new_article=Post.objects.create(
+    #             postname=request.POST['postname'],
+    #             contents=request.POST['contents'],
+    #             mainphoto=request.POST['mainphoto'],
+    #         )
+    #     return redirect('/blog/')
+# text 분석 함수 post
+def get_contents(request):
+    if request.method == "POST":
+        new_diary = Diary.object.create(
+            diary_id = 1,
+            title = '오늘은 즐거운 날이다.',
+            contents = '피아노를 치는데 소리가 구리다. 자괴감이 대박이다.',
+            is_deleted = False
+        )
+
+    # db 연결 코드 추가
+    # 플라스크로 넘기기
+    keyword_json = get_keyword(new_diary.contents)
+    # 결과 받기
+    return HttpResponse("hello! this is get_keyword api")
+
+
+# text 기반 그림 보여주기
+# 1. 선택한 키워드 받기
+# 2. 키워드 기반으로 db에서 이미지 불러오기
+# 3. 반환
