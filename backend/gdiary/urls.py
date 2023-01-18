336c1acb44542e23ea573716ea56ee9627db7c4d
Heekyeong Park <qkrgmlrud00@gmail.com>
2023-01-18 12:24:01 -0800

    fix: 회원가입, 로그인 관련 url 수정

--- backend/gdiary/urls.py
+++ backend/gdiary/urls.py
@@ -10,9 +10,9 @@ router.register(r'diaries', DiaryViewset)
 # postman으로 router에 있는 것을 테스트할 때 '/' 유무 안 중요함
 # postman으로 urlpatterns에 있는 것을 테스트할 때 '/' 유무 중요함
 urlpatterns = [
+    path('', include(router.urls)),
+
     path('join/', RegisterAPIView.as_view()), #회원가입
     path("auth/refresh/", TokenRefreshView.as_view()), #토큰 재발급
     path('auth/', AuthAPIView.as_view()), #로그인
-    path('', include(router.urls)),
-
 ]
