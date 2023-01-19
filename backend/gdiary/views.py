d382215ff764a8ca6fcf688d6f9175eb6dd9d3c8
Heekyeong Park <qkrgmlrud00@gmail.com>
2023-01-19 02:13:00 -0800

    feat: swagger에서 query string 추가

--- backend/gdiary/views.py
+++ backend/gdiary/views.py
@@ -2,11 +2,35 @@ from rest_framework import viewsets, permissions, generics, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 from config.settings import *
 from .serializers import *
 from .models import *
 
 class DiaryViewset(viewsets.ModelViewSet):
     queryset = Diary.objects.all()
-    serializer_class = DiarySerializer   
+    serializer_class = DiarySerializer 
 
+    # manual parameter
+    param_date = openapi.Parameter(
+        'diary_date',
+        openapi.IN_QUERY,
+        description='yyyy-mm-dd',
+        type=openapi.FORMAT_DATE
+    )
+
+    @swagger_auto_schema(manual_parameters=[param_date])
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
+   
+    def get_queryset(self):
+        diaries = Diary.objects.filter(is_deleted = False)
+
+        date = self.request.query_params.get('date', '')
+        if date:
+            diaries = diaries.filter(diary_date=date)
+        return diaries
+
+    
