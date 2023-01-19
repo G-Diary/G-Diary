62dc50f0b6cbc7d04b49e201fa1dd974c7c76b37
Johnyun <108129068+yunyoungse2222@users.noreply.github.com>
2023-01-20 02:10:52 +0900

    style: 로그인 후 화면 수정
    화면 css 수정

--- frontend/src/components/Modal/AfterLogin.js
+++ frontend/src/components/Modal/AfterLogin.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import Modal from 'react-modal';
 import BookCover from '../bookshape/BookCover';
 import Modals from './Modal'
+import Titles from '../../pages/Title';
+import CoverControl from '../../pages/CoverControl';
+
 
 function AfterLogin(){
   return (
