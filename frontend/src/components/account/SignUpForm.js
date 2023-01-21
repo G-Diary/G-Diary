1d3cadcbf5c2273f21cdfaf29681a4c054c78ec2
gs0428 <lider28@naver.com>
2023-01-22 02:36:56 +0900

    feat : 회원가입 API 구현

--- frontend/src/components/account/SignUpForm.js
+++ frontend/src/components/account/SignUpForm.js
@@ -1,7 +1,8 @@
 import React, {useState} from 'react';
+import {useNavigate} from 'react-router-dom'
 import styled from 'styled-components'
 import {Button, Container, TextField} from '@material-ui/core';
-import api from '../../apis/axios';
+import api from '../../apis/axios'
 
 const TypeSignUp = styled.div`
 position: relative;
@@ -22,12 +23,11 @@ const Wrap = styled.div`
   align-items: center;`
 
 function SignUpForm() {
+  const navigate = useNavigate();
   const [nickname, setNickname] = useState('');
   const [email, setEmail] = useState('');
-  const [pw, setPw] = useState('');
-  const [cf, setCf] = useState('');
-  const [isNameDup, setIsNameDup] = useState(true);
-  const [isEmailDup, setIsEmailDup] = useState(true);
+  const [password, setPassword] = useState('');
+  const [confirm, setConfirm] = useState('');
   const Swal = require('sweetalert2');
 
   function nameInput(e) {
@@ -54,66 +54,56 @@ function SignUpForm() {
     return check.test(email);
   }
 
-  function pwValid() {
+  function passwordValid() {
     var check = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,25}$/;
-    return check.test(pw);
+    return check.test(password);
   }
 
-  function Same() {
-    if(cf === pw)
+  function isSame() {
+    if(confirm === password)
       return true;
     else
       return false;
   }
 
-  function onSubmit() {
-    Swal.fire({
-      position: 'center',
-      icon: 'success',
-      title: 'Success SignUp',
-      showConfirmButton: false,
-      timer: 2000
+  function onClick(e) {
+    e.preventDefault();
+    api.post('/join/', {
+      nickname: `${nickname}`,
+      email: `${email}`,
+      password: `${password}`
+    }).then(function(res) {
+      Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: 'Success SignUp',
+        showConfirmButton: false,
+        timer: 2000
+      })
+      navigate('/signin')
+    }).catch(function(res) {
+      if(res.response.data.email) {
+        Swal.fire({
+          position: 'center',
+          icon: 'error',
+          title: `${res.response.data.email}`,
+          showConfirmButton: false,
+          timer: 2000
+        })
+      } else if(res.response.data.nickname) {
+        Swal.fire({
+          position: 'center',
+          icon: 'error',
+          title: `${res.response.data.nickname}`,
+          showConfirmButton: false,
+          timer: 2000
+        })
+      }
     })
   }
   
-  async function onClick(e) {
-    e.preventDefault();
-    await api.post('/join', {
-      nickname: `${nickname}`,
-      email: `${email}`
-    }).then(function(res) {
-      console.log(res.data)
-      if(!res.data.nickname) {
-        Swal.fire({
-          position: 'center',
-          icon: 'warning',
-          title: 'Exist nickname',
-          showConfirmButton: false,
-          timer: 2000
-        })  
-      } else if(!res.data.email) {
-        Swal.fire({
-          position: 'center',
-          icon: 'warning',
-          title: 'Exist email',
-          showConfirmButton: false,
-          timer: 2000
-        })  
-      } else {
-        Swal.fire({
-          position: 'center',
-          icon: 'success',
-          title: 'Success',
-          showConfirmButton: false,
-          timer: 2000
-        })  
-      }
-    }).catch()
-   
-  }
-
   function Valid() {
-    if((nickname ? true : false) & (nickname.length >= 2) & !nicknameValid() & emailValid() & pwValid() & Same()){
+    if((nickname ? true : false) & (nickname.length >= 2) & !nicknameValid() & emailValid() & passwordValid() & isSame()){
       return false;
     } else return true;
   }
@@ -121,93 +111,86 @@ function SignUpForm() {
   return(
     <Wrap>
       <CreateAccountBtn>
-        <Button form='sub' type='submit' disabled={Valid()}
+        <Button type='button' onClick={onClick} disabled={Valid()}
           style={ !Valid() ? {fontWeight:'bolder', backgroundColor: '#FFD711', borderRadius: '30px', fontSize: '30px'} : { color:'white',fontWeight:'bolder',backgroundColor: '#F8EDB7',borderRadius: '30px', fontSize: '30px'}}>
         Create Account</Button>
       </CreateAccountBtn>
-      <form id="sub" action='/signin' onSubmit={onSubmit}>
-        <TypeSignUp>
-          <Container maxWidth='sm'>
-            <TextField
-              margin="dense"   
-              fullWidth
-              variant="filled"
-              required
-              label="Nickname"
-              name="Nickname"
-              autoComplete="freeSolo"
-              autoFocus
-              value={nickname}
-              onChange={nameInput}
-              error={nicknameValid()}
-              helperText={
-                nicknameValid() ? 'Special characters are not allowed.' : ''
-              }
-            />
-            <CheckDuplicate>
-            </CheckDuplicate>
-            <TextField
-              margin="dense"
-              fullWidth
-              variant="filled"
-              required
-              label="Email"
-              name="email"
-              autoComplete="email"
-              type='text'
-              value={email}
-              onChange={(e) => {
-                setEmail(e.target.value)
-              }}
-              error={email ? !emailValid() : emailValid()}
-              helperText={
-                email ? (!emailValid() ? 'Enter it in e-mail format.' : '') : ''
-              }
-            />
-            <TextField
-              margin="dense"
-              fullWidth
-              variant="filled"
-              required
-              type="password"
-              label="Password"
-              name="Password"
-              autoComplete="new-password"
-              value={pw}
-              onChange={(e) => {
-                setPw(e.target.value)
-              }}
-              error={pw ? !pwValid() : pwValid()}
-              helperText={
-                pw ? (!pwValid() ? 'Enter at least 8 digits, including numbers and special characters.' : '') : ''
-              }
-            />    
-            <TextField
-              margin="dense"
-              fullWidth
-              variant="filled"
-              required
-              type="password"
-              label="Confirm"
-              name="Confirm"
-              autoComplete="new-password"
-              value={cf}
-              onChange={(e) => {
-                setCf(e.target.value)
-              }}
-              error={cf ? (!cf ? Same() : !Same()) : false}
-              helperText={
-                cf ? (!Same() ? 'Check your password again.' : '') : ''
-              }
-            />
-            <Button type='button' onClick={onClick} style={{
-              float: 'right',
-              backgroundColor:'#FFD711',
-              fontWeight: 'bolder'
-            }}>✔︎</Button>
-          </Container>
-        </TypeSignUp>
-      </form>
+      <TypeSignUp>
+        <Container maxWidth='sm'>
+          <TextField
+            margin="dense"   
+            fullWidth
+            variant="filled"
+            required
+            label="Nickname"
+            name="Nickname"
+            autoComplete="freeSolo"
+            autoFocus
+            value={nickname}
+            onChange={nameInput}
+            error={nicknameValid()}
+            helperText={
+              nicknameValid() ? 'Special characters are not allowed.' : ''
+            }
+          />
+          <CheckDuplicate>
+          </CheckDuplicate>
+          <TextField
+            margin="dense"
+            fullWidth
+            variant="filled"
+            required
+            label="Email"
+            name="email"
+            autoComplete="email"
+            type='text'
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value)
+            }}
+            error={email ? !emailValid() : emailValid()}
+            helperText={
+              email ? (!emailValid() ? 'Enter it in e-mail format.' : '') : ''
+            }
+          />
+          <TextField
+            margin="dense"
+            fullWidth
+            variant="filled"
+            required
+            type="password"
+            label="Password"
+            name="Password"
+            autoComplete="new-password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value)
+            }}
+            error={password ? !passwordValid() : passwordValid()}
+            helperText={
+              password ? (!passwordValid() ? 'Enter at least 8 digits, including numbers and special characters.' : '') : ''
+            }
+          />    
+          <TextField
+            margin="dense"
+            fullWidth
+            variant="filled"
+            required
+            type="password"
+            label="Confirm"
+            name="Confirm"
+            autoComplete="new-password"
+            value={confirm}
+            onChange={(e) => {
+              setConfirm(e.target.value)
+            }}
+            error={confirm ? (!confirm ? isSame() : !isSame()) : false}
+            helperText={
+              confirm ? (!isSame() ? 'Check your password again.' : '') : ''
+            }
+          />
+        </Container>
+      </TypeSignUp>
     </Wrap>
   );
 }
