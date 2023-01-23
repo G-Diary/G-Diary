bfe1a3042c8af37cede123e47e2cf7e356031976
Minji Lee <79428205+alswlfl29@users.noreply.github.com>
2023-01-23 23:38:30 +0900

    feat: 그림판 기능(이동및 크기조절)

--- frontend/src/components/diary/Drawing.js
+++ frontend/src/components/diary/Drawing.js
@@ -1,326 +1,252 @@
-import React, {useRef, useEffect, useState} from 'react';
+import React, {useState, useEffect, useRef, Fragment} from 'react';
+import {Stage, Layer, Line, Image, Transformer} from 'react-konva';
 import { useStore } from '../../store/store';
-import { BsEraserFill } from 'react-icons/bs';
+import useImage from 'use-image';
 import {BsFillCircleFill, BsFillEraserFill } from 'react-icons/bs';
 import { FaUndoAlt } from 'react-icons/fa';
-import {Stage, Layer, Line, Image, Transformer} from 'react-konva';
-import useImage from 'use-image';
 
-const defaultStyle={
-  display: 'inline-block',
-}
-
-function Drawing({grim, save}){
-  const [ctx, setCtx] = useState();  //canvas
-  const canvasRef = useRef(null);
-  const [isDrawing, setIsDrawing] = useState(!{grim}); //그리기 모드 여부판단
-  const {setCurrentCanvas,setUpdateCanvas}=useStore();
-  const [tool, setTool]=useState('pen');
-  const [lines,setLines]=useState([]);
-  const [currentColor,setColor]=useState('black');
-  const listColors=['black','red','blue']
-  console.log(currentColor);
-
+const Rectangle = ({ image, shapeProps, isSelected, onSelect, onChange }) => {
+  const shapeRef = useRef();
+  const trRef = useRef();
+  const [img] = useImage(image,'Anonymous');
   useEffect(() => {
-    const canvas = canvasRef.current;
-    const context = canvas.getContext('2d');
-    context.lineWidth = 2;
-    context.lineJoin = 'round';
-    setCurrentCanvas(context); 
-    setUpdateCanvas(canvasRef.current);
-    setCtx(context);
-  }, []);
+    if (isSelected) {
+      // we need to attach transformer manually
+      trRef.current.nodes([shapeRef.current]);
+      trRef.current.getLayer().batchDraw();
+    }
+  }, [isSelected]);
 
-  //그림 그리기 동작 모드 ON
-  const startDrawing = ({ nativeEvents }) => {
-    setIsDrawing(true);
-  };
-  //그림 그리기 동작 모드 OFF
-  const finishDrawing = () => {
-    setIsDrawing(false);
-  };
+  return (
+    <Fragment>
+      <Image
+        image={img}
+        onClick={onSelect}
+        onTap={onSelect}
+        ref={shapeRef}
+        {...shapeProps}
+        draggable
+        onDragEnd={(e) => {
+          onChange({
+            ...shapeProps,
+            x: e.target.x(),
+            y: e.target.y()
+          });
+        }}
+        onTransformEnd={(e) => {
+          const node = shapeRef.current;
+          const scaleX = node.scaleX();
+          const scaleY = node.scaleY();
+          node.scaleX(1);
+          node.scaleY(1);
+          onChange({
+            ...shapeProps,
+            x: node.x(),
+            y: node.y(),
+            // set minimal value
+            width: Math.max(5, node.width() * scaleX),
+            height: Math.max(node.height() * scaleY)
+          });
+        }}
+      />
+      {isSelected && (
+        <Transformer
+          ref={trRef}
+          boundBoxFunc={(oldBox, newBox) => {
+            // limit resize
+            if (newBox.width < 5 || newBox.height < 5) {
+              return oldBox;
+            }
+            return newBox;
+          }}
+        />
+      )}
+    </Fragment>
+  );
+};
 
-  //그림 그리기
-  const drawing = ({ nativeEvent }) => {
-    const { offsetX, offsetY } = nativeEvent;
-    if (ctx) {
-      if (!isDrawing) {
-        ctx.beginPath();
-        ctx.moveTo(offsetX, offsetY);
-        ctx.strokeStyle=currentColor;
-        ctx.lineWidth=2;
-        // eslint-disable-next-line no-lone-blocks, no-unused-expressions
-        {tool==='eraser'?(ctx.globalCompositeOperation='destination-out', ctx.lineWidth=8):ctx.globalCompositeOperation ='source-over';}
-      } else {
-        ctx.lineTo(offsetX, offsetY);
-        ctx.stroke();
-      }
+function Drawing({grim}){
+  const {choiceImg, setUpdateCanvas}=useStore();
+  const [grimimage, setGrimimage] = useState(choiceImg);
+  const [selectedId, selectShape] = useState(null);
+  const [tool, setTool] = useState('pen');
+  const [currentColor,setColor]=useState('#000000');
+  const listColors=['black','red','blue']
+  const [lines, setLines] = useState([]);
+  let stageRef=useRef(null);
+  const isDrawing = useRef(false);
+  const checkDeselect = (e) => {
+    // deselect when clicked on empty area
+    const clickedOnEmpty = e.target === e.target.getStage();
+    if (clickedOnEmpty) {
+      selectShape(null);
     }
   };
-
-  //캔버스 위 마우스 클릭 좌표 확인
-  const clickXY = (e) => {
-    const x = e.clientX - e.target.offsetLeft;
-    console.log(x);
-    const y = e.clientY - e.target.offsetTop;
-    console.log(y);
+  
+  useEffect(() => {
+    setGrimimage([...grimimage, choiceImg]);
+  }, [choiceImg]);
+  const handleMouseDown = (e) => {
+    // debugger;
+    isDrawing.current = true;
+    const pos = e.target.getStage().getPointerPosition();
+    setLines([...lines, { tool, points: [pos.x, pos.y],color:currentColor }]);
+  };
+  const handleMouseMove = (e) => {
+    if (!isDrawing.current) {
+      return;
+    }
+    const stage = e.target.getStage();
+    const point = stage.getPointerPosition();
+    let lastLine = lines[lines.length - 1];
+    // add point
+    lastLine.points = lastLine.points.concat([point.x, point.y]);
+    // replace last
+    lines.splice(lines.length - 1, 1, lastLine);
+    setLines(lines.concat());
+  };
+  const handleMouseUp = () => {
+    isDrawing.current = false;
+  };
+  const handleUndo = () => {
+    setLines(lines.slice(0, -1));
   };
 
-  //그림 이미지화
-  const saveAsPNG = () => {
-    // const image = canvasRef.current.toDataURL('image/png');
-    // console.log(image);
-    // console.log(image.width);
-    save(canvasRef.current);
-  };
-  return( <div style={{width:'512px', height: '270px'}}>
-    {!grim ? (
-      <>
-        <canvas
-          id="canvas"
-          className='canvas'
-          width="498" 
-          height="270"
-          ref={canvasRef}
-          style={defaultStyle}
-          onMouseDown={startDrawing}
-          onMouseUp={finishDrawing}
-          onMouseMove={drawing}
-          onMouseLeave={()=>{
-            finishDrawing();
-            saveAsPNG();}}
-        ></canvas>
-        <div style={{ display:'flex', justifyContent:'flex-end',alignContent:'center', transform:'translate(0,-90%)', marginRight:'10px'}}>
+  const handleExport = () =>{
+    const dataUrl=stageRef.current.toDataURL({
+      mimeType:'image/png',
+      quality:0,
+      pixelRatio:2,
+      crossorigin:'anonymous'
+    });
+    setUpdateCanvas(dataUrl);
+  }
+
+  return(
+    <div>
+      {/* <button onClick={handleExport}>save</button> */}
+      {grim?(
+        <Stage
+          ref={stageRef}
+          width={500}
+          height={270}
+          onMouseDown={(e) => {      
+            checkDeselect(e);
+          }}
+          onTouchStart={(e) => {
+            checkDeselect(e);
+          }}
+          onMouseLeave={handleExport}
+        >
+          <Layer>
+            {grimimage && grimimage.map((rect, i) => {
+              return (
+                <Rectangle
+                  key={i}
+                  image={rect.img}
+                  shapeProps={rect}
+                  isSelected={rect.id === selectedId}
+                  onSelect={() => {
+                    selectShape(rect.id);
+                  }}
+                  onChange={(newAttrs) => {
+                    const rects = grimimage.slice();
+                    rects[i] = newAttrs;
+                    setGrimimage(rects);
+                  }}
+                />
+              );
+            })}
+            {lines && lines.map((line, i) => (
+              <Line
+                key={i}
+                points={line.points}
+                stroke={line.color}
+                strokeWidth={5}
+                tension={0.5}
+                lineCap="round"
+                globalCompositeOperation={
+                  line.tool === 'eraser' ? 'destination-out' : 'source-over'
+                }
+              />
+            ))}
+          </Layer>
+        </Stage>
+      ):( <Stage
+        ref={stageRef}
+        width={500}
+        height={270}
+        onMouseDown={(e) => {      
+          handleMouseDown(e);
+          checkDeselect(e);
+        }}
+        onTouchStart={(e) => {
+          handleMouseDown(e);
+          checkDeselect(e);
+        }}
+        onMousemove={handleMouseMove}
+        onMouseup={handleMouseUp}
+        onTouchMove={(e) => {
+          handleMouseMove(e);
+        }}
+        onTouchEnd={(e) => {
+          handleMouseUp(e);
+        }}
+        onMouseLeave={handleExport}
+      >
+        <Layer>
+          {grimimage && grimimage.map((rect, i) => {
+            return (
+              <Rectangle
+                key={i}
+                image={rect.img}
+                shapeProps={rect}
+                isSelected={rect.id === selectedId}
+                onSelect={() => {
+                  selectShape(rect.id);
+                }}
+                onChange={(newAttrs) => {
+                  const rects = grimimage.slice();
+                  rects[i] = newAttrs;
+                  setGrimimage(rects);
+                }}
+              />
+            );
+          })}
+          {lines && lines.map((line, i) => (
+            <Line
+              key={i}
+              points={line.points}
+              stroke={line.color}
+              strokeWidth={5}
+              tension={0.5}
+              lineCap="round"
+              globalCompositeOperation={
+                line.tool === 'eraser' ? 'destination-out' : 'source-over'
+              }
+            />
+          ))}
+        </Layer>
+      </Stage>)}
+      {!grim?(
+        <div style={{transform:'translate(68%,-100%)'}}>
           {listColors && listColors.map((map,index)=>{
             return(
               <BsFillCircleFill key={index} color={map} size="23" style={{marginRight:'8px'}} onClick={()=>{
+                setTool('pen');
                 setColor(map);
-                setTool('pencil')
               }} />
             )
           })}
-          <BsFillEraserFill size="24" style={{marginRight: '10px', marginBottom: '10px'}} onClick={()=>{setTool('eraser')}}/>
-        </div>
-      </>
-    ) : (
-      <canvas
-        id="canvas"
-        className="canvas"
-        width="498" 
-        height="270"
-        ref={canvasRef}
-        style={defaultStyle}
-        onClick={clickXY}
-      ></canvas>
-    )}
+          <BsFillEraserFill size="26" style={{marginRight: '10px'}} onClick={()=>{
+            setTool('eraser');
+          }} />
+          <FaUndoAlt size="22" style={{marginRight:'10px'}} onClick={handleUndo}/>
+        </div>):('')}
+    
+    </div>
+  )
 
-  </div>)
 
-  
 }
-
 export default Drawing;
-
-
-// {lines.map((map, i)=>(
-//   <Line
-//     key={i}
-//     points={map.points}
-//     stroke={map.color}
-//     strokeWidth={5}
-//     tension={0.5}
-//     lineCap="round"
-//     globalCompositeOperation={
-//       map.tool==='eraser' ?'destination-out':'source-over'
-//     }
-//   />
-// ))}
-
-
-
-
-// const [tool, setTool]=useState('pen');
-//   const [lines,setLines]=useState([]);
-//   // const [savedDrawing, setSavedDrawing]=useState('');
-//   const [currentColor,setColor]=useState('#000000');
-//   const listColors=['black','red','blue']
-//   const isDrawing=useRef(false);
-//   let {choiceImg, setCurrentCanvas}=useStore();
-//   const [selected, setSelected]=useState(false);
-//   let stageRef=useRef(null);
-//   const trRef = useRef();
-//   console.log(choiceImg);
- 
-//   const [image]=useImage(choiceImg.src)
-//   console.log(image);
-//   const [shape, setShape]=useState({
-//     width: 80,
-//     height:80,
-//     x:10,
-//     y:10
-//   });
-
-//   const [crop, setCrop] =useState({
-//     width: 80,
-//     height:80,
-//     x:10,
-//     y:10
-//   });
-
-//   useEffect(()=>{
-//     if(selected){
-//       trRef.current?.nodes([stageRef.current]);
-//       trRef.current?.getLayer()?.batchDraw();
-//     }
-//   },[selected]);
-
-//   const getCrop=(image,newSize)=>{
-//     const aspectRatio=newSize.width/newSize.height;
-
-//     const imageRatio=image.width/image.height;
-
-//     let newWidth=aspectRatio>=imageRatio?image.width:image.height*aspectRatio;
-//     let newHeight=aspectRatio>=imageRatio?image.width/aspectRatio:image.height;
-//     let x=(image.width-newWidth)/2;
-//     let y=(image.height-newHeight)/2;
-
-//     return{
-//       x:x,
-//       y:y,
-//       width:newWidth,
-//       height:newHeight
-//     };
-//   };
-
-//   const transformer=(end)=>{
-//     const node=stageRef.current;
-
-//     const scaleX = node.scaleX();
-//     const scaleY = node.scaleY();
-
-//     let newWidth=Math.max(5,node.width()*scaleX);
-//     let newHeight=Math.max(node.height()*scaleY);
-
-//     if(end===false){
-      
-//     }else{
-//       node?.width(newWidth);
-//       node?.height(newHeight);
-//       const crop=getCrop(
-//         {width:image?.width, height:image?.height},
-//         {width:newWidth, height:newHeight}
-//       );
-//       node.crop(crop);
-//     }
-//   };
-
-
-//   const handleMouseDown = (e)=>{
-//     // debugger;
-//     const pos=e.target.getStage().getPointerPosition();
-//     setLines([...lines, {tool, points:[pos.x,pos.y],color:currentColor}]);
-//     isDrawing.current=true;
-//     console.log(lines)
-//   };
-
-//   const handleMouseMove= (e)=>{
-//     if(!isDrawing.current){
-//       return;
-//     }
-//     const stage=e.target.getStage();
-//     const point=stage.getPointerPosition();
-//     console.log(point);
-//     let lastLine=lines[lines.length-1];
-//     lastLine.color=currentColor;
-//     lastLine.points = lastLine.points.concat([point.x, point.y]);
-//     lines.splice(lines.length-1,1,lastLine);
-//     setLines(lines.concat());
-//     console.log(lines);
-//   }
-//   const handleMouseUp = () =>{
-//     isDrawing.current=false;
-//     setCurrentCanvas(stageRef)
-//   }
-
-//   // const handleExport = () =>{
-//   //   const dataUrl=stageRef.toDataURL();
-//   //   setSavedDrawing(dataUrl);
-//   // }
-
-//   const handleUndo = () =>{
-//     setLines(lines.slice(0,-1));
-//   }
- 
-//   return( 
-//     <>
-//       <div>
-//         <Stage 
-//           ref={stageRef}
-//           width={498}
-//           height={243}
-//           onMouseDown={handleMouseDown}
-//           onMouseMove={handleMouseMove}
-//           onMouseup={handleMouseUp}
-//           onTouchStart={(e)=>{
-//             console.log(e);
-//             handleMouseDown(e);
-//           }}
-//           onTouchMove={(e)=>{
-//             handleMouseMove(e);
-//           }}
-//           onTouchEnd={(e)=>{
-//             handleMouseUp(e);
-//           }}
-//         >
-//           <Layer>
-//             <Image
-//               ref={stageRef}
-//               y={shape.y}
-//               x={shape.x}
-//               image={image}
-//               scaleX={1}
-//               scaleY={1}
-//               width={shape.width}
-//               height={shape.height}
-//               crop={getCrop(
-//                 {width: image?.width, height:image?.height},
-//                 {width:crop.width, height:crop.height}
-//               )}
-//               onClick={()=>setSelected(!selected)}
-//               onTransform={(e)=>{
-//                 transformer(false);
-//               }}
-//               draggable={true}
-//               rotation={30}
-              
-//             />
-//             <Transformer
-//               ref={trRef}
-//               resizeEnabled={true}
-//               rotateEnabled={true}
-//               boundBoxFunc={(oldBox,newBox)=>{
-//                 if(newBox.width<5 || newBox.height<5){
-//                   return oldBox;
-//                 }
-//                 return newBox;
-//               }}
-//             />
-//           </Layer>
-//         </Stage>
-//       </div>
-//       <div style={{ display:'flex', justifyContent:'flex-end',alignContent:'center'}}>
-//         {listColors && listColors.map((map,index)=>{
-//           return(
-//             <BsFillCircleFill key={index} color={map} size="23" style={{marginRight:'8px'}} onClick={()=>{
-//               setTool('pen');
-//               setColor(map);
-//             }} />
-//           )
-//         })}
-//         <FaUndoAlt size="23" style={{marginRight:'10px'}} onClick={handleUndo}/>
-//         {/* <BsEraserFill size="27" style={{marginRight: '10px', Bottom:'10px'}} onClick={()=>{
-//           setTool('eraser');
-//         }} /> */}
-//       </div>
-//     </>
-//   )
--- frontend/src/components/diary/GrimChoice.js
+++ frontend/src/components/diary/GrimChoice.js
@@ -2,31 +2,30 @@ import React from 'react';
 import styled from 'styled-components';
 import { useStore } from '../../store/store';
 function GrimChoice(){
-  const {currentCanvas, setChoiceImg}=useStore();
+  const {currentCanvas,setChoiceImg}=useStore();
   const addImage = (srcImg) => {
     const newimage = new Image();
-    newimage.src = srcImg.src;
+    newimage.src=srcImg.src;
+    console.log(newimage);
     newimage.crossOrigin = 'Anonymous';
-    newimage.onload = function () {
-      currentCanvas.drawImage(newimage, 0, 0, srcImg.width, srcImg.height);
-    };
+    // newimage.onload = function () {
+    //   currentCanvas.drawImage(newimage, 0, 0, srcImg.width, srcImg.height);
+    // };
+    setChoiceImg(
+      {
+        id:srcImg.alt,
+        img: newimage.src,
+        x:0,
+        y:0,
+        width: srcImg.width,
+        height: srcImg.height,
+      })
   };
 
   const onChange = (e) => {
     e.preventDefault();
     addImage(e.target);
   };
- 
-  
-  // const onChange = (e) => {
-  //   e.preventDefault();
-  //   addImage(e.target);
-  // };
-  
-  // const addImage = (srcImg) => {
-  //   // const newimage = new Image();
-  //   setChoiceImg(srcImg);
-  // };
   
   return(
     <ChoiceContainer>
@@ -40,7 +39,6 @@ function GrimChoice(){
           alt="heart" onClick={onChange}/>
       </Choice>
       <ChoiceButtonContainer>
-        <Choicebutton>Grim</Choicebutton>
         <Choicebutton>Photo</Choicebutton>
       </ChoiceButtonContainer>
     </ChoiceContainer>)
