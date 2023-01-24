2687234d33c8d94cb6bd8a1abd3535cc04ff61e6
Johnyun <108129068+yunyoungse2222@users.noreply.github.com>
2023-01-24 20:14:29 +0900

    feat:select btn 추가, @@'s Diary title 추가

--- frontend/src/components/Modal/AfterLogin.js
+++ frontend/src/components/Modal/AfterLogin.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Modal from 'react-modal';
 import BookCover from '../bookshape/BookCover';
+import BookCoverJ from '../bookshape/BookCoverJ';
 import Modals from './Modal'
 import Titles from '../../pages/Title';
 import CoverControl from '../../pages/CoverControl';
@@ -9,9 +10,9 @@ import CoverControl from '../../pages/CoverControl';
 
 function AfterLogin(){
   return (
-    <BookCover>
+    <BookCoverJ>
       <Modals/>
-    </BookCover>
+    </BookCoverJ>
   );
 }
 
---
+++ frontend/src/components/bookshape/BookCoverJ.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import styled from 'styled-components';
+import Bookmark from '../diary/Bookmark';
+
+const AllControl = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;`
+
+const Left = styled.div`
+  background-color: #F0DB6D;
+  float: left;
+  width: 55px;
+  height: 758px;
+  border-top-left-radius: 20px;
+  border-bottom-left-radius: 20px;
+  box-shadow:16px 9px 10px 0 #676262;`
+
+const Right = styled.div`
+  background-color: #FDF6EA;
+  float: left;
+  width: 675px;
+  height: 758px;
+  border-top-right-radius: 30px;
+  border-bottom-right-radius: 30px;
+  box-shadow:12px 9px 10px 0 #676262;`
+
+const Year = styled.div`
+  border-bottom: solid 2px black;
+  font-size: 30px;
+  margin: 50px;
+  margin-bottom: 0`
+
+
+function BookCover({children}) {
+  let now = new Date();
+  let year = now.getFullYear();
+  return (
+    <AllControl>
+      <Left/>
+      <Right>
+        <Year>{year}</Year>
+        {children}
+      </Right>
+      <Bookmark/>
+    </AllControl>
+  );
+}
+
+export default BookCover;
