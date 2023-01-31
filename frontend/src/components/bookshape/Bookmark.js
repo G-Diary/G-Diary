30ee68d6af6ebf9dc2646dd9cc2baeb58906ebd8
gs0428 <lider28@naver.com>
2023-01-31 17:52:41 +0900

    fix : 충돌 방지 수정

--- frontend/src/components/bookshape/Bookmark.js
+++ frontend/src/components/bookshape/Bookmark.js
@@ -1,49 +1,76 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation, useNavigate } from 'react-router-dom';
 import IsLogin from '../access/IsLogin';
-import { useStore } from '../../store/store';
 
 const BookMark = styled.div`
-    width: 70px;
-    height: 700px;
-    z-index:80;
-`
-const StyledNavLink=styled(NavLink)`
-    display: block;
-    width: 65px;
-    height: 43px;
+  width: 70px;
+  height: 700px;
+  z-index: 1;
+`;
+const StyledNavLink = styled(NavLink)`
+  display: block;
+  width: 65px;
+  height: 43px;
+  text-decoration: none;
+  margin-bottom: 10px;
+  font-size: 18px;
+  border-radius: 0 5px 5px 0;
+  text-align: center;
+  line-height: 43px;
+  color: white;
+  border: 0.8px groove gray;
+  border-left: none;
+  font-weight: 400;
+  opacity: 0.9;
+  background-color: rgba(0, 0, 0, 0.8);
+  &:link {
+    transition: 0.5s;
     text-decoration: none;
-    margin-bottom: 10px;
-    font-size: 18px;
-    border-radius: 0 5px 5px 0;
-    text-align: center;
-    line-height: 43px;
+  }
+  &:hover {
+    color: gray;
+  }
+  &.active {
+    background-color: #f0db6d;
     color: black;
-    border: 0.8px groove gray;
-    border-left: none;
-    font-weight: 400;
-    opacity:0.9;
-    &:link {
-      transition : 0.5s;
-      text-decoration: none;
+    font-weight: 700;
+  }
+`;
+
+function Bookmark() {
+  const location = useLocation();
+  let loca = location.pathname;
+
+  useEffect(() => {
+    if (loca === '/signin' || loca === '/signup' || (IsLogin() && loca === '/')) {
+      document.getElementById('home').style.backgroundColor = '#F0DB6D';
+      document.getElementById('home').style.color = 'black';
+      document.getElementById('home').style.fontWeight = '700';
     }
-    &:hover{
-      color: gray;
+  }, []);
+
+  function Valid() {
+    if (loca === '/' || loca === '/signin' || loca === '/signup' || (!IsLogin() && loca === '/about')) {
+      return 'none';
+    } else {
+      return '';
     }
-    &.active {
-      color: #d5260d;
-      font-weight: 700;
-    }
-`
-function Bookmark(){
-  const {setChoiceImg,setGetGrimList}=useStore();
-  return(
+  }
+
+  return (
     <BookMark>
-      <StyledNavLink to={IsLogin() ? '/main' : '/'} style={{background:'#80FF00'}}>홈</StyledNavLink> 
-      <StyledNavLink to={'/list'} style={{background:'#FFE600'}} onClick={()=>{setChoiceImg(''); setGetGrimList(''); }}>일기쓰기</StyledNavLink> 
-      <StyledNavLink to='/about' style={{ background: 'rgba(217, 132, 57)' }}>소개</StyledNavLink>
-    </BookMark>)
+      <StyledNavLink to={IsLogin() ? '/main' : '/'}>
+        <div id='home' style={{ borderRadius: '0 5px 5px 0' }}>
+          홈
+        </div>
+      </StyledNavLink>
+      <StyledNavLink id='write' to='/list' style={{ pointerEvents: Valid() }}>
+        일기 쓰기
+      </StyledNavLink>
+      <StyledNavLink to={'/about'}>소개</StyledNavLink>
+    </BookMark>
+  );
 }
 
 export default Bookmark;
--- frontend/src/components/diarylist/ResultManuscript.js
+++ frontend/src/components/diarylist/ResultManuscript.js
@@ -1,24 +1,82 @@
+import { Button, makeStyles } from '@material-ui/core';
 import React from 'react';
-import { PaperContainer, TableTd } from '../diary/Manuscript';
+import { PaperContainer, TableTd } from './Manuscript';
+import api from '../../apis/axios';
+
+const useStyles = makeStyles((theme) => ({
+  customHoverFocus: {
+    '&:hover, &.Mui-focusVisible': { backgroundColor: 'black', color: 'white' },
+  },
+}));
+
 /* 리스트 원고지 틀 컴포넌트 */
-function ResultManuscript({content}) {
-  const divi=content.split('');
+function ResultManuscript({ content }) {
+  const classes = useStyles();
+  const divi = content.split('');
   let tr = Array.apply(null, new Array(5)).map(Number.prototype.valueOf, 0);
   let td = Array.apply(null, new Array(10)).map(Number.prototype.valueOf, 0);
-  const textlist = tr.map((tr,index) => (
-    <div style={{display: 'flex'}} key={index}>
-      {td.map((td,index1) => (
-        <TableTd key={index1}><div style={{paddingTop:'5px'}}>{divi[index1 + 10 * index]}</div></TableTd>
+  const textlist = tr.map((tr, index) => (
+    <div style={{ display: 'flex' }} key={index}>
+      {td.map((td, index1) => (
+        <TableTd key={index1}>
+          <div style={{ paddingTop: '5px' }}>{divi[index1 + 10 * index]}</div>
+        </TableTd>
       ))}
     </div>
   ));
 
+  function shareMessage() {
+    api
+      .get(`diaries/${sessionStorage.getItem('id')}`)
+      .then(function (res) {
+        console.log(res);
+        window.Kakao.Share.sendDefault({
+          objectType: 'feed',
+          content: {
+            title: '내 일기 어때?',
+            description: '너도 쓰러 와!',
+            // 일기에서 그린 그림 url 주소 하고 싶어영
+            imageUrl: res.data.drawing_url,
+            link: {
+              // 도메인 주소 정해지면 그거 넣으면 될 것 같아여
+              mobileWebUrl: 'http://localhost:3000/',
+              webUrl: 'http://localhost:3000/',
+            },
+          },
+        });
+      })
+      .catch(function (res) {});
+  }
   return (
-    <div style={{marginTop:'9px', marginLeft:'7px'}}>
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'flex-end',
+        position: 'relative',
+        bottom: '25.5px',
+        marginLeft: '7px',
+      }}
+    >
+      <Button
+        onClick={shareMessage}
+        className={classes.customHoverFocus}
+        type='button'
+        variant='outlined'
+        style={{
+          position: 'relative',
+          bottom: '3.5px',
+          right: '10px',
+          borderRadius: '30px',
+          border: '2px solid black',
+          fontWeight: 'bolder',
+        }}
+      >
+        카카오톡 공유하기
+      </Button>
       <PaperContainer>
-        <label>
-          {textlist}
-        </label>
+        <label>{textlist}</label>
       </PaperContainer>
     </div>
   );
