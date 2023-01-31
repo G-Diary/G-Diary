7c05a9270cdfffb3d0e0042cd3f2ee9937df6194
aristo <dkfud2121@naver.com>
2023-01-31 16:28:40 +0900

    error : current status, model save() error

---
+++ backend/text/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import *
+
+class ResultSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Result
+        #모두 직렬화하겠음
+        fields = '__all__'
--- backend/text/views.py
+++ backend/text/views.py
@@ -4,12 +4,16 @@ import time
 from konlpy.tag import Kkma
 from requests import Response
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+
 from config.celery import *
 from rest_framework.response import Response
 from rest_framework import status
 from config.decode import decode
 import django
 from rest_framework.utils import json
+
+from text.models import Result
 
 django.setup()
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
@@ -19,7 +23,6 @@ kkma = Kkma()
 def get_keyword(request):
     body =  json.loads(request.body.decode('utf-8'))
     contents = body["contents"]
-    print("   contents >>> ", contents)
     diary_keyword = decode.delay(contents)
 
     while True:
@@ -28,6 +31,13 @@ def get_keyword(request):
             print("    delay...    ")
             continue
         else :
+            for word in diary_keyword.get() :
+                # keyword_model = Result(keyword=word)
+                # keyword_model.save()
+                keyword_model=Result.objects.all()
+                keyword_model.keyword=word
+                keyword_model.save()
+
             print(diary_keyword.get())
             print(diary_keyword.ready())
             return Response({
