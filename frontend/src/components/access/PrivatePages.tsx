ceb20af7cb2873cfa00c074fc3db2572fdfef9ee
gs0428 <lider28@naver.com>
2023-04-19 14:11:47 +0900

    refactor:any타입 수정

--- frontend/src/components/access/PrivatePages.tsx
+++ frontend/src/components/access/PrivatePages.tsx
@@ -1,8 +1,15 @@
 import SignIn from '../../pages/SignIn'
 import IsLogin from './IsLogin'
 
-export default function PrivatePages({ Component} : any ) {
+type Props = {
+  Component: React.ComponentType<{}>; // Component가 React 컴포넌트인 경우, any 대신에 해당 컴포넌트의 Props 타입을 명시하는 것이 더 안전함
+};
+
+
+const PrivatePages: React.FC<Props> = ({ Component }) => {
   return (
-    IsLogin() ? <Component /> : <SignIn />
+    IsLogin() ? <Component/> : <SignIn />
   )
-}
+};
+
+export default PrivatePages;
